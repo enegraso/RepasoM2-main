119810ef87586c231e66c87de5866d2a2fb75cb3
enegraso <sib2000@gmail.com>
2021-04-20 23:25:56 -0300

    subiendo para la notebook

--- src/App.js
+++ src/App.js
@@ -7,18 +7,40 @@ const myCities={
 };
 
 
-export const reducer=(state,action) => {
+export const reducer=(state = myCities,action) => {
+  switch(action.type){
+    case 'AddCity':
+    return {
+          ...state, cities: state.cities.push(action.payload)
+    } 
+
+    case 'RemoveCity':
+      return state.cities.filter(item => item.id === action.payload.id)
+      
+    
+    default:
+      return state
+
+  }
 
   
 }
 
 // ACTIONS -----------------------
 // por como esta diseñado el test, el id de la ciudad deberian colocarla al momento de agregarlo en el reducer
- export const addCity= ()=>{
-  
+ export const addCity = (arg) => {
+   let idcity = 1
+   return {
+   type: "AddCity",
+    payload: {...arg, id: idcity+1}
+   }
  }
 
- export const removeCity=()=>{
+ export const removeCity = (payload) => {
+   return {
+    type: "RemoveCity",
+   payload: {id: payload} 
+  }
  }
 
 
@@ -31,9 +53,42 @@ export const reducer=(state,action) => {
  // Recuerden que la idea es practicar y tener conceptos claros, con que entiendan los tests y sientan que entendieron
  // es suficiente.
 export const App=() => {
+  const [state, useState] = React.useState({
+    city: '',
+    location: '',
+    temperatura: ''
+  })
+
+  // e: nombre del campo del formulario que se esté cambiando
+  const HandleChange = (e) => {              
+    useState({
+      ...state,
+      [e.target.name]: e.target.value
+    })
+  }
+
 
   return(
-    <h1>Repaso</h1>
+    <div>
+      <form>
+        <div>
+      <label>City</label>
+      <input name="city" value={state.city} onChange={HandleChange}></input>
+      </div>
+      <div>
+      <label>Location</label>
+      <textarea name="location"  value={state.location} onChange={HandleChange}></textarea>
+      </div>
+      <div>
+      <label>Temperature</label>
+      <input name="temperatura"  value={state.temperatura} onChange={HandleChange} ></input>
+      </div>
+      <div>
+        <button type="submit"/*  onClick={reducer(state, addCity(payload))} */>Agregar Ciudad</button> 
+        </div>
+      </form>
+    </div>
+
   )
 
 }
